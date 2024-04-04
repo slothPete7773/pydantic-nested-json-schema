0da0627f9028196a4347eb2b4fd7f2575c79cee8
slothPete7773 <Veerakit413@gmail.com>
2024-04-05 00:05:07 +0700

    feat: add local file option

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 from models import Student
+import json
 
-url = "https://raw.githubusercontent.com/bugbytes-io/datasets/master/students_v3.json"
-response = requests.get(url)
-data = response.json()
+
+# Option 1: Download file from Source
+# url = "https://raw.githubusercontent.com/bugbytes-io/datasets/master/students_v3.json"
+# response = requests.get(url)
+# data = response.json()
+
+# Option 2: Open file from local file (Same file)
+with open("student_v3.json") as file:
+    data = json.load(file)
+
 
 for student in data:
     # print(student)
